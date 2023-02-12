94b525c6a7de13411a219b9de77c726601454dc8
Liadenia <liden207@naver.com>
2023-02-13 03:38:23 +0900

    add movement
    page movement finish

--- js/profile.js
+++ js/profile.js
@@ -4,12 +4,36 @@ const scroll=()=>{
     if(scrollTop>=document.querySelector('#page1').offsetTop){
         document.querySelector('.main_info').style.left=`-${offset1}px`;
     }
-    // let offset2 = scrollTop - document.querySelector('#page2').offsetTop;
-    // console.log(scrollTop, offset2);
-    // if(scrollTop>=document.querySelector('#page2').offsetTop){
-    //     document.querySelector('.main_info').style.left=`-${offset2}px`;
-    // }
+
+    let offset2 = scrollTop - document.querySelector('#page5').offsetTop;
+    console.log(scrollTop, offset2);
+    if(scrollTop>=document.querySelector('#page5').offsetTop){
+        document.querySelector('#page5').style.left=`+${offset2}px`;
+    }
+
+    
+
+
+    // document.querySelector('#page5').style.left = `-${100}px`;
+    // let offset = scrollTop - document.querySelector('#page5').offsetTop;
+    // document.querySelector('#page5').style.left = `-${offset}px`;
+
 }
 window.addEventListener('scroll',()=>(
     scroll()
 ))
+
+
+
+
+$(function(){
+    $('.contact').on('click', function(){
+        $('.con-txt, .con-bg').toggleClass('on');
+        $('.con-bg').fadeToggle(400)
+    });
+
+    $('.con-bg').on('click', function(){
+        $('.con-txt, .con-bg').removeClass('on');
+        $('.con-bg').fadeOut(400)
+    });
+});
