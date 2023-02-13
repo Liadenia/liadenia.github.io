fbff3001439f8ed7cbb0e06a8985ee361248469d
Liadenia <121140535+Liadenia@users.noreply.github.com>
2023-02-13 15:04:40 +0900

    change tag
    change movement

--- js/profile.js
+++ js/profile.js
@@ -1,31 +1,56 @@
+// -----------------------------첫번째 페이지 가로 스크롤-----------------------
 const scroll=()=>{
     let scrollTop = window.pageYOffset;
     let offset1 = scrollTop - document.querySelector('#page1').offsetTop;
     if(scrollTop>=document.querySelector('#page1').offsetTop){
         document.querySelector('.main_info').style.left=`-${offset1}px`;
     }
-
+// ----------------------------마지막 페이지 가로 스크롤------------------------
     let offset2 = scrollTop - document.querySelector('#page5').offsetTop;
     console.log(scrollTop, offset2);
     if(scrollTop>=document.querySelector('#page5').offsetTop){
         document.querySelector('#page5').style.left=`+${offset2}px`;
     }
 
+    // document.querySelector('#portfolio').addEventListener('click', function(event) {
+    //     event.preventDefault();
+    //     let portfolioS = document.querySelector('#portfolio-s').offsetTop;
+    //     window.scrollTo({
+    //         top: portfolioS,
+    //         behavior: 'smooth'
+    //     });
+    //     console.log('#portfolio');
+    // });
+
+
+    const portfolio = document.querySelector("#portfolio");
+    portfolio.addEventListener("click", function(event) {
+        event.preventDefault();
+        const portfolioS = document.querySelector("#portfolio-s");
+        portfolioS.scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    });
+
+
+
     
-
-
-    // document.querySelector('#page5').style.left = `-${100}px`;
-    // let offset = scrollTop - document.querySelector('#page5').offsetTop;
-    // document.querySelector('#page5').style.left = `-${offset}px`;
-
 }
 window.addEventListener('scroll',()=>(
     scroll()
 ))
 
+// document.querySelector('#portfolio').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     document.querySelector('#portfolio-s').scrollIntoView({ 
+//     behavior: 'smooth' 
+//     });
+//     console.log('#portfolio')
+// });
 
 
 
+//-------------------------------contact GNB 클릭시 토글 페이지 보이기----------
 $(function(){
     $('.contact').on('click', function(){
         $('.con-txt, .con-bg').toggleClass('on');
@@ -37,3 +62,4 @@ $(function(){
         $('.con-bg').fadeOut(400)
     });
 });
+
